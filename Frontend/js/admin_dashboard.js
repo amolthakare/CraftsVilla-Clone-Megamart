a76585b015ca35422e0876837c777ba53fb488cb
amolthakare <100188653+amolthakare@users.noreply.github.com>
2023-01-23 02:37:35 +0530

    admin side added

---
+++ Frontend/js/admin_dashboard.js
@@ -0,0 +1,121 @@
+let token = localStorage.getItem("login");
+const register = document.getElementById("register");
+register.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const image = document.getElementById("image_post").value;
+  const name = document.getElementById("name_post").value;
+  const price = document.getElementById("price_post").value;
+  const strike = "";
+  const discount = "";
+
+  fetch("http://localhost:4500/data/create", {
+    method: "POST",
+    body: JSON.stringify({ image, name, price, strike, discount }),
+    headers: {
+      authenticate: `${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        console.log(data);
+      } else {
+        console.log("err");
+      }
+      alert("Product has been added");
+      window.location.reload();
+    });
+});
+
+const displaycart = async () => {
+  try {
+    let res = await fetch(`http://localhost:4500/data`, {
+      headers: {
+        authenticate: `${token}`,
+      },
+    });
+    let data = await res.json();
+    console.log(data);
+    bag = data;
+    console.log("bag", bag);
+    display(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+displaycart();
+function display(data){
+    document.querySelector("tbody").innerText="";
+    data.map((elem)=>{
+        let row = document.createElement("tr");
+
+        let img = document.createElement("td");
+        img.setAttribute("class","image");
+        let image = document.createElement("img");
+        image.src=elem.image;
+
+        img.append(image);
+
+        let name = document.createElement("td");
+        name.innerText=elem.name;
+        name.setAttribute("class","name");
+
+        let price = document.createElement("td");
+        price.innerText="₹ "+elem.price;
+        price.setAttribute("class","price");
+
+        let strike = document.createElement("td");
+        strike.innerText=elem.strike;
+        strike.setAttribute("class","strike");
+
+        let discount = document.createElement("td");
+        discount.innerText=elem.discount;
+        discount.setAttribute("class","discount");
+
+        let deletebtn = document.createElement("td");
+        deletebtn.innerText="Delete";
+        deletebtn.setAttribute("class","deletebtn");
+        deletebtn.addEventListener("click", function () {
+            Delete(elem._id);
+          });
+
+        let editbtn = document.createElement("td");
+        editbtn.innerText="Edit";
+        editbtn.setAttribute("class","editbtn");
+        editbtn.addEventListener("click", function () {
+            item(elem);
+        });
+
+        row.append(img,name,price,strike,discount,deletebtn,editbtn);
+        document.querySelector("tbody").append(row);
+    })
+}
+
+const Delete = async (id) => {
+    try {
+      let res = await fetch(`http://localhost:4500/data/delete/${id}`, {
+        headers: {
+          authenticate: `${token}`,
+        },
+        method: "DELETE",
+      });
+      let data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  
+    displaycart();
+};
+
+const item=(elem)=>{
+    let patch = elem;
+    localStorage.setItem("patch",JSON.stringify(patch));
+    window.location="./admin_patch.html"
+}
+
+let home = document.getElementById("home");
+home.addEventListener("click",()=>{
+    window.location="../index.html";
+})
---
+++ Frontend/js/admin_patch.js
@@ -0,0 +1,28 @@
+let data = JSON.parse(localStorage.getItem("patch"));
+console.log(data._id);
+let token = localStorage.getItem("login");
+const register = document.getElementById("register");
+register.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const image = document.getElementById("image_post").value;
+  const name = document.getElementById("name_post").value;
+  const price = document.getElementById("price_post").value;
+
+  fetch(`http://localhost:4500/data/update/${data._id}`, {
+    method: "PATCH",
+    body: JSON.stringify({ image, name, price,}),
+    headers: {
+      authenticate: `${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+    //   if (data.success) {
+    //     console.log(data);
+    //   }
+    console.log(data);  
+    });
+    alert("Product has been updated");
+    window.location="./admin_dashboard.html";
+});
